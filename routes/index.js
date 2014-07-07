b61cdbd1703f72aa50aec9796bb2cd11fe5d0ecf
Manoj Chalode <mchalode@tavisca.com>
2014-07-07 18:59:46 +0530

    added project stuff

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/getUser', function (req,res) {
+	var db = req.db;
+	var collection = db.get('users');
+	collection.find({},{},function (e, docs) {
+		res.render('user', {users : docs});
+	});
+});
+
+module.exports = router;
